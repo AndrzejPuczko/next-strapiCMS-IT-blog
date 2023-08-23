9433942a0e68ebe88ee48a5bb7ecdb2f5b636d6a
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-08-23 20:15:05 +0200

    feat: add homepage and layout for it

---
+++ src/app/layout.jsx
@@ -0,0 +1,32 @@
+import { Lato, Open_Sans } from 'next/font/google'
+import '@/styles/styles.scss'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+const lato = Lato({
+	subsets: ['latin'],
+	weight: ['400', '700', '900'],
+	variable: '--font-lato',
+})
+const openSans = Open_Sans({
+	subsets: ['latin'],
+	variable: '--font-open-sans',
+})
+export const metadata = {
+	title: 'Początek przygody z technologią - Blog IT',
+	description: 'Odkryj fascynujący świat technologii. Od Windows Server po hostowanie stron - zanurz się w najnowszych trendach i rozwiązaniach informatycznych!',
+}
+
+const RootLayout = ({ children }) => {
+	return (
+		<html lang="pl">
+			<body className={`${openSans.className} ${openSans.variable} ${lato.variable}`}>
+				<Header /> 
+				{children}
+				<Footer />
+			</body>
+		</html>
+	)
+}
+
+export default RootLayout
---
+++ src/app/page.jsx
@@ -0,0 +1,55 @@
+import Card from '@/components/card/Card'
+import { IconTypes } from '@/components/button/Button'
+import labels from '@/helpers/labels'
+
+const Home = () => (
+	<div className="container pb-80">
+		<Card
+			label={labels.productReviews}
+			title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+			summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam perferendis dicta vel rem voluptate, consectetur unde ratione fuga libero, ducimus incidunt! Vitae unde
+					quasi sae"
+			href="#"
+			btnIcon={IconTypes.ARROW_RIGHT}
+      className='mb-30'
+		/>
+
+		<div className="row">
+			<div className="col col_4 m-mw-100">
+				<Card
+					label={labels.opinion}
+					title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+					summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam perferendis dicta vel rem voluptate, consectetur unde ratione fuga libero, ducimus incidunt! Vitae unde
+					quasi sae"
+					href="#"
+					btnIcon={IconTypes.ARROW_RIGHT}
+          className="mb-30"
+				/>
+			</div>
+			<div className="col col_4 m-mw-100">
+				<Card
+					label={labels.productReviews}
+					title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+					summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam perferendis dicta vel rem voluptate, consectetur unde ratione fuga libero, ducimus incidunt! Vitae unde
+					quasi sae"
+					href="#"
+					btnIcon={IconTypes.ARROW_RIGHT}
+          className="mb-30"
+				/>
+			</div>
+			<div className="col col_4 m-mw-100">
+				<Card
+					label={labels.guide}
+					title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+					summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam perferendis dicta vel rem voluptate, consectetur unde ratione fuga libero, ducimus incidunt! Vitae unde
+					quasi sae"
+					href="#"
+					btnIcon={IconTypes.ARROW_RIGHT}
+          className="mb-30"
+				/>
+			</div>
+		</div>
+	</div>
+)
+
+export default Home
