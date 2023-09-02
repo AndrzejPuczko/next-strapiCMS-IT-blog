7abb015bc8456346050a8beab9bae500f3e28ccd
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-09-02 20:46:30 +0200

    feat: add cms sorting and filtering function

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,68 +3,49 @@ import { IconTypes } from '@/components/button/Button'
 import labels from '@/helpers/labels'
 import config from '@/config'
 
-const fetchBlogs = async () => {
+const fetchBlogs = async params => {
 	const reqOptions = {
 		headers: {
 			Authorization: `Bearer ${process.env.API_TOKEN}`,
 		},
 	}
-	const request = await fetch(`${config.api}/api/blogs?populate=*&filters[isFeatured][$eq]=true`, reqOptions)
+	const request = await fetch(`${config.api}/api/blogs?populate=*${params}`, reqOptions)
 	const response = await request.json()
 
 	return response
 }
 
 const Home = async () => {
-	const blogs = await fetchBlogs()
-	console.log(blogs.data)
+	const [featuredBlogs, blogs] = await Promise.all([await fetchBlogs('&filters[isFeatured][$eq]=true'), await fetchBlogs('&sort=createdAt:desc&filters[isFeatured][$eq]=false')])
 
-	return ( 
+	return (
 		<div className="container pb-80">
-			<Card
-				label={labels.productReviews}
-				title={blogs.data[0].attributes.Title}
-				summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam perferendis dicta vel rem voluptate, consectetur unde ratione fuga libero, ducimus incidunt! Vitae unde
-					quasi sae"
-				href="#"
-				btnIcon={IconTypes.ARROW_RIGHT}
-				className="mb-30"
-			/>
+			{featuredBlogs.data.map(featuredBlog => (
+				<Card
+					key={featuredBlog.id}
+					label={featuredBlog.attributes.Category}
+					title={featuredBlog.attributes.Title}
+					summary={featuredBlog.attributes.Summary}
+					href={`/${featuredBlog.attributes.slug}`}
+					btnIcon={IconTypes.ARROW_RIGHT}
+					className="mb-30"
+				/>
+			))}
 
 			<div className="row">
-				<div className="col col_4 m-mw-100">
-					<Card
-						label={labels.opinion}
-						title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-						summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam perferendis dicta vel rem voluptate, consectetur unde ratione fuga libero, ducimus incidunt! Vitae unde
-					quasi sae"
-						href="#"
-						btnIcon={IconTypes.ARROW_RIGHT}
-						className="mb-30"
-					/>
-				</div>
-				<div className="col col_4 m-mw-100">
-					<Card
-						label={labels.productReviews}
-						title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-						summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam perferendis dicta vel rem voluptate, consectetur unde ratione fuga libero, ducimus incidunt! Vitae unde
-					quasi sae"
-						href="#"
-						btnIcon={IconTypes.ARROW_RIGHT}
-						className="mb-30"
-					/>
-				</div>
-				<div className="col col_4 m-mw-100">
-					<Card
-						label={labels.guide}
-						title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-						summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam perferendis dicta vel rem voluptate, consectetur unde ratione fuga libero, ducimus incidunt! Vitae unde
-					quasi sae"
-						href="#"
-						btnIcon={IconTypes.ARROW_RIGHT}
-						className="mb-30"
-					/>
-				</div>
+				{blogs.data.map(featuredBlog => (
+					<div className="col col_4 m-mw-100" key={featuredBlog.id}>
+						<Card
+							key={featuredBlog.id}
+							label={featuredBlog.attributes.Category}
+							title={featuredBlog.attributes.Title}
+							summary={featuredBlog.attributes.Summary}
+							href={`/${featuredBlog.attributes.slug}`}
+							btnIcon={IconTypes.ARROW_RIGHT}
+							className="mb-30"
+						/>
+					</div>
+				))}
 			</div>
 		</div>
 	)
