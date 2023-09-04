4fa2ac9832c35856b7facfabcbc595d8b585014f
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-09-04 15:07:48 +0200

    feat: add fetchBlogs function to load props in single slug page

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,41 +1,47 @@
 import Image from 'next/image'
 import getCategoryColor from '@/helpers/get-category-color'
-import labels from '@/helpers/labels'
+import ReactMarkdown from 'react-markdown'
 import styles from './style.module.scss'
+import fetchBlogs from '@/helpers/fetch-blogs'
+import config from '@/config'
 
-const BlogDetails = () => {
+const BlogDetails = async props => {
+	const blogs = await fetchBlogs(`&filters[slug][$eq]=${props.params.slug}`)
+
+	if (blogs.data.length === 0) return null
+
+	const blog = blogs.data[0]
+
 	return (
 		<main className="container pb-80">
 			<div className="row mb-50">
 				<div className="col col_9">
-					<div className={`h6 mb-20 c-${getCategoryColor(labels.productReviews)}`}>{'Product reviews'}</div>
-					<h2 className="mb-50">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
-					<Image className={`${styles.featuredImage} mb-50`} src="/thumb.jpg" alt="technologia blog" width="1280" height="520" />
+					<div className={`h6 mb-20 c-${getCategoryColor(blog.attributes.Category)}`}>{blog.attributes.Category}</div>
+					<h2 className="mb-50">{blog.attributes.Title}</h2>
+					<Image
+						className={`${styles.featuredImage} mb-50`}
+						src={`${config.api}${blog.attributes.FeaturedImage.data.attributes.url}`}
+						alt="technologia blog"
+						width="1280"
+						height="520"
+						priority
+					/>
 				</div>
 			</div>
 			<div className="row">
 				<div className="col col_9">
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab enim cumque magnam optio quibusdam at omnis dolorum cum et libero minus quae aperiam odio obcaecati placeat
-						in, consectetur natus. IlloLorem ipsum dolor sit amet consectetur adipisicing elit. Ab enim cumque magnam optio quibusdam at omnis dolorum cum et libero minus quae
-						aperiam odio obcaecati placeat in, consectetur natus. Illo!!
-					</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum totam velit in laboriosam recusandae facere quas cumque, quasi, officiis similique arLorem ipsum dolor sit
-						amet consectetur adipisicing elit. Ab enim cumque magnam optio quibusdam at omnis dolorum cum et libero minus quae aperiam odio obcaecati placeat in, consectetur natus.
-						Illo!chitecto dignissimos fugit aperiam fuga dolorem iusto voluptatem quisquam delectus expedita! Pariatur quam quia fugiat dolore vitae dicta inventore, amet fugit,
-						deserunt perferendis minus facere.
-					</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias repellaLorem ipsum dolor sit amet consectetur adipisicing elit. Ab enim cumque magnam optio quibusdam at
-						omnis dolorum cum et libero minus quae aperiam odio obcaecati placeat in, consectetur natus. Illo!Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab enim
-						cumque magnam optio quibusdam at omnis dolorum cum et libero minus quae aperiam odio obcaecati placeat in, consectetur natus. Illo!t nostrum at eos cum velit inventore
-						explicabo saepe iste expedita.
-					</p>
+					<ReactMarkdown>{blog.attributes.Content}</ReactMarkdown>
 				</div>
 			</div>
 		</main>
 	)
+}
+
+export const generateStaticParams = async () => {
+	const blogs = await fetchBlogs()
+	return blogs.data.map(blog => ({
+		slug: blog.attributes.slug,
+	}))
 }
 
 export default BlogDetails
